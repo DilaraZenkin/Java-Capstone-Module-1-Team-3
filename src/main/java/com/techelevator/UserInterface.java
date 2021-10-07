288c57347d6d28918ff5b1b1270cdd1cbcd592b0
dilarazenkin <dilaraznkn@gmail.com>
2021-10-07 13:34:57 -0400

    1.34pm

---
+++ src/main/java/com/techelevator/UserInterface.java
@@ -0,0 +1,18 @@
+package com.techelevator;
+
+public class UserInterface {
+
+    public void displayMainMenu() {
+        System.out.println("(1) Display Vending Machine Items");
+        System.out.println("(2) Purchase");
+        System.out.println("(3) Exit");
+    }
+
+    public void displayPurchaseMenu() {
+
+        System.out.println("(1) Feed Money");
+        System.out.println("(2) Select Product");
+        System.out.println("(3) Finish Transaction");
+    }
+
+}
